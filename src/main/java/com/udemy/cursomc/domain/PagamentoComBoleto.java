bae7d586c71621c331420bb4870d3caf6d3f9097
Guilherme Carvalho <guilherme_230308@hotmail.com>
2018-04-29 14:16:38 -0300

    Endpoint /pedidos/id Disponibilizado

--- src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
@@ -3,13 +3,16 @@ package com.udemy.cursomc.domain;
 import java.util.Date;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.udemy.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
+	@JsonFormat(pattern="dd/MM/yyy")
 	private Date dataVencimento;
+	@JsonFormat(pattern="dd/MM/yyy")
 	private Date dataPagamento;
 
 	public PagamentoComBoleto() {
