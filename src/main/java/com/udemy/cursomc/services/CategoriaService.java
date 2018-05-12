27d70c17e996410e6505ada1ba1a49c06b14a2ec
Guilherme Carvalho <guilherme_230308@hotmail.com>
2018-05-12 14:48:56 -0300

    Testando a conexão com o Git

--- src/main/java/com/udemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -1,12 +1,18 @@
 package com.udemy.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursomc.domain.Categoria;
 import com.udemy.cursomc.repository.CategoriaRepository;
+import com.udemy.cursomc.services.exception.DataIntegrityException;
 import com.udemy.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -20,7 +26,7 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
@@ -29,5 +35,23 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possuí produtos");
+		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
